d86459a5888fe7211f8d4d2615ec98c7860cee38
ycy <ya70201@gmail.com>
2018-08-10 14:54:09 +0800

    add voice activity detection template.

--- utils.py
+++ utils.py
@@ -1,5 +1,15 @@
 import numpy as np
 from torchaudio.transforms import MuLawEncoding, MuLawExpanding
+from webrtcvad import Vad
+from librosa.util import frame
+
+
+def voice_unvoice(x, sr, winstep=0.01, winlen=0.03, mode=3):
+    vad = Vad(mode)
+    windowsize = int(sr * winlen)
+    hopsize = int(sr * winstep)
+    x = np.pad(x, (windowsize // 2,) * 2, 'constant')
+    frames = frame(x, frame_length=windowsize, hop_length=hopsize)
 
 
 def encoder(quantization_channels):
@@ -23,7 +33,6 @@ def zero_padding(x, maxlen, dim=0):
                 pad_shape += ((0, diff),)
 
         return np.pad(x, pad_shape, 'constant')
-
 
 
 if __name__ == '__main__':
