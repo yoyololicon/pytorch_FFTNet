1b3caa663912f01d0580fb9e988b245b1e4625bb
ycy <ya70201@gmail.com>
2018-08-31 15:09:00 +0800

    update training file to new version.

--- train.py
+++ train.py
@@ -4,21 +4,21 @@ import torch.backends.cudnn as cudnn
 import argparse
 import os
 
-from preprocess import preprocess_multi
+from preprocess import preprocess_cmu
 from models import general_FFTNet
 from dataset import CMU_Dataset
 from datetime import datetime
 
-parser = argparse.ArgumentParser(description='FFTNet vocoder training.')
+parser = argparse.ArgumentParser()
 parser.add_argument('--preprocess', action='store_true')
-parser.add_argument('--wav_dir', type=str, default='/host/data_dsk1/dataset/CMU_ARCTIC_Databases/cmu_us_rms_arctic/wav')
-parser.add_argument('--data_dir', type=str, default='training_data')
+parser.add_argument('--wav_dir', type=str, default='/host/data_dsk1/dataset/CMU_ARCTIC_Databases/cmu_us_slt_arctic/wav')
+parser.add_argument('--data_dir', type=str, default='slt_mcc_data')
 parser.add_argument('--feature_type', type=str, default='mcc')
 parser.add_argument('--feature_dim', type=int, default=25, help='number of mcc coefficients')
-parser.add_argument('--mcep_alpha', type=float, default=0.42, help='all-pass filter constant. '
-                                                                   '16khz: 0.42; '
-                                                                   '10khz: 0.35; '
-                                                                   '8khz: 0.31.')
+parser.add_argument('--mcep_alpha', type=float, default=0.42, help='''all-pass filter constant.
+                                                                   16khz: 0.42,
+                                                                   10khz: 0.35,
+                                                                   8khz: 0.31.''')
 parser.add_argument('--window_length', type=float, default=0.025)
 parser.add_argument('--window_step', type=float, default=0.01)
 parser.add_argument('--minimum_f0', type=float, default=71)
@@ -29,35 +29,36 @@ parser.add_argument('--fft_channels', type=int, default=128, help='fftnet layer 
 parser.add_argument('--seq_M', type=int, default=5000, help='training sequence length')
 parser.add_argument('--radixs', nargs='+', type=int, default=[2] * 11)
 parser.add_argument('--batch_size', type=int, default=5)
-parser.add_argument('--lr', type=float, default=0.004, help='learning rate')
+parser.add_argument('--lr', type=float, default=0.001, help='learning rate')
 parser.add_argument('--steps', type=int, default=100000, help='iteration number')
-parser.add_argument('--noise_injecting', type=bool, default=True)
-parser.add_argument('--model_file', type=str, default='fftnet_model')
+parser.add_argument('--injected_noise', action='store_true')
+parser.add_argument('--model_file', type=str, default='slt_fftnet')
 parser.add_argument('--checkpoint_dir', type=str, default='checkpoints/',
                     help='Directory to save checkpoints.')
 parser.add_argument('--checkpoint_step', type=int, default=5000)
 parser.add_argument('--transpose', action='store_true')
-
-sampling_rate = 16000
+parser.add_argument('--predict_dist', type=int, default=1)
 
 
 def main():
     args = parser.parse_args()
     if args.preprocess:
         print('==> Preprocessing data ...')
-        preprocess_multi(args.wav_dir, args.data_dir, winlen=args.window_length, winstep=args.window_step,
-                         n_mcep=args.feature_dim, mcep_alpha=args.mcep_alpha, minf0=args.minimum_f0,
-                         maxf0=args.maximum_f0, type=args.feature_type)
+        preprocess_cmu(args.wav_dir, args.data_dir, q_channels=args.q_channels, winlen=args.window_length,
+                       winstep=args.window_step, n_mcep=args.feature_dim, mcep_alpha=args.mcep_alpha,
+                       minf0=args.minimum_f0, maxf0=args.maximum_f0, type=args.feature_type)
 
     print('==> Loading Dataset..')
-    training_dataset = CMU_Dataset(args.data_dir, args.seq_M, args.q_channels, int(sampling_rate * args.window_step),
-                                   args.interp_method, injected_noise=args.noise_injecting)
+    training_dataset = CMU_Dataset(args.data_dir, args.seq_M, args.q_channels, int(16000 * args.window_step),
+                                   args.interp_method, injected_noise=args.injected_noise,
+                                   predict_dist=args.predict_dist)
     training_loader = DataLoader(training_dataset, batch_size=args.batch_size, num_workers=4, shuffle=True)
 
     print('==> Building model..')
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
     net = general_FFTNet(radixs=args.radixs, fft_channels=args.fft_channels, classes=args.q_channels,
-                         aux_channels=args.feature_dim + 1, transpose=args.transpose).to(device)
+                         aux_channels=args.feature_dim + 1, transpose=args.transpose,
+                         predict_dist=args.predict_dist).to(device)
 
     if torch.cuda.device_count() > 1:
         net = torch.nn.DataParallel(net)
