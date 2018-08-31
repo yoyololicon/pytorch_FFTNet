353f728e619b249721ed9ca83ddf351782e8f7d5
ycy <ya70201@gmail.com>
2018-08-31 15:09:51 +0800

    add post synthesis denoise to decode file.

--- decode.py
+++ decode.py
@@ -1,26 +1,26 @@
 import torch
-import argparse
+from torch.nn import functional as F
+from tqdm import tqdm
 import os
-from librosa.core import load
-from sklearn.preprocessing import StandardScaler
+import argparse
 import numpy as np
 from torchaudio import save
-from datetime import datetime
+from sklearn.preprocessing import StandardScaler
 from scipy.interpolate import interp1d
-
-from utils import decoder
+from datetime import datetime
+from utils import decoder, logmmse, vad
 from preprocess import get_features
 
-parser = argparse.ArgumentParser(description='FFTNet decoder.')
+parser = argparse.ArgumentParser()
 parser.add_argument('--infile', type=str, default=None)
 parser.add_argument('--outfile', type=str, default=None)
-parser.add_argument('--data_dir', type=str, default='training_data')
+parser.add_argument('--data_dir', type=str, default='slt_mcc_data')
 parser.add_argument('--feature_type', type=str, default='mcc')
 parser.add_argument('--feature_dim', type=int, default=25, help='number of mcc coefficients')
-parser.add_argument('--mcep_alpha', type=float, default=0.42, help='all-pass filter constant.'
-                                                                   '16khz: 0.42,'
-                                                                   '10khz: 0.35,'
-                                                                   '8khz: 0.31.')
+parser.add_argument('--mcep_alpha', type=float, default=0.42, help='''all-pass filter constant.
+                                                                   16khz: 0.42,
+                                                                   10khz: 0.35,
+                                                                   8khz: 0.31.''')
 parser.add_argument('--window_length', type=float, default=0.025)
 parser.add_argument('--window_step', type=float, default=0.01)
 parser.add_argument('--minimum_f0', type=float, default=71)
@@ -28,8 +28,10 @@ parser.add_argument('--maximum_f0', type=float, default=800)
 parser.add_argument('--q_channels', type=int, default=256, help='quantization channels')
 parser.add_argument('--interp_method', type=str, default='linear')
 parser.add_argument('-c', type=float, default=2., help='a constant multiply before softmax.')
-parser.add_argument('--model_file', type=str, default='fftnet_model.pth')
+parser.add_argument('--model_file', type=str, default='slt_fftnet.pth')
 parser.add_argument('--cuda', action='store_true')
+parser.add_argument('--denoise', action='store_true')
+parser.add_argument('--noise_std', type=float, default=0.005)
 
 sampling_rate = 16000
 
@@ -54,7 +56,7 @@ if __name__ == '__main__':
             # haven't implement
             pass
         elif args.outfile is not None:
-            id, _, h = get_features(args.infile, winlen=args.window_length, winstep=args.window_step,
+            id, x, h = get_features(args.infile, winlen=args.window_length, winstep=args.window_step,
                                     n_mcep=args.feature_dim, mcep_alpha=args.mcep_alpha, minf0=args.minimum_f0,
                                     maxf0=args.maximum_f0, type=args.feature_type)
 
@@ -72,18 +74,37 @@ if __name__ == '__main__':
                 exit(1)
 
             h = torch.from_numpy(h).unsqueeze(0).float()
+            r_field = net.get_receptive_field()
+            pred_dist = net.get_predict_distance()
+
+            vad_curve = vad(torch.from_numpy(x), hopsize).numpy()
+            vad_curve = np.repeat(vad_curve, hopsize)
+
+            output_buf = torch.empty(h.size(2)).long()
+            h = F.pad(h, (r_field, 0))
+            samples = torch.zeros(pred_dist).long()
             if args.cuda:
                 h = h.cuda()
+                samples = samples.cuda()
 
+            net.init_buf()
             print("Decoding file", args.infile)
             a = datetime.now().replace(microsecond=0)
-            generation = net.fast_generate(h=h, c=args.c, method='sampling')
+            for pos in tqdm(range(r_field + pred_dist, h.size(2) + 1, pred_dist)):
+                out_pos = pos - r_field - pred_dist
+                decision = np.mean(vad_curve[out_pos:out_pos + pred_dist])
+                if decision > 0.5:
+                    samples = net.one_sample_generate(samples, h=h[:, :, :pos], c=args.c)
+                else:
+                    samples = net.one_sample_generate(samples, h=h[:, :, :pos])
 
-            dec = decoder(args.q_channels)
-            generation = dec(generation)
-            save(args.outfile, generation, sampling_rate)
+                output_buf[out_pos:out_pos + pred_dist] = samples
             cost = datetime.now().replace(microsecond=0) - a
-            print("Generation time cost:", cost)
+            dec = decoder(args.q_channels)
+            generation = dec(output_buf)
+            if args.denoise:
+                generation = logmmse(generation, sampling_rate, noise_std=args.noise_std)
+            save(args.outfile, generation.view(-1, 1), sampling_rate)
             print("Speed:", generation.size(0) / cost.total_seconds(), "samples/sec.")
         else:
             print("Please enter output file name.")
